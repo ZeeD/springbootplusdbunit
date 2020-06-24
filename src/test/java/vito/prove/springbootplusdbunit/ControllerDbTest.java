3a4bb440445be1a317c6f0120324fcf33f78f399
Vito De Tullio <vito.detullio.fincons@skytv.it>
2020-06-24 16:37:53 +0200

    PrepAndExpectedTestCase autowired

--- src/test/java/vito/prove/springbootplusdbunit/ControllerDbTest.java
+++ src/test/java/vito/prove/springbootplusdbunit/ControllerDbTest.java
@@ -11,11 +11,12 @@ import org.dbunit.VerifyTableDefinition;
 import org.dbunit.operation.DatabaseOperation;
 import org.dbunit.util.fileloader.CsvDataFileLoader;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 
 import lombok.val;
@@ -26,40 +27,44 @@ public class ControllerDbTest {
     @Autowired
     MyRestController controller;
     @Autowired
-    DataSource ds;
-    PrepAndExpectedTestCase testCase;
-
-    @BeforeEach
-    void setup() throws Throwable {
-        val databaseTester =
-                           new DataSourceDatabaseTester(new TransactionAwareDataSourceProxy(this.ds));
-        databaseTester.setTearDownOperation(DatabaseOperation.DELETE_ALL);
-        this.testCase =
-                      new DefaultPrepAndExpectedTestCase(new CsvDataFileLoader(),
-                                                         databaseTester);
-    }
+    PrepAndExpectedTestCase dbUnit;
 
     @Test
     void myTest() throws Throwable {
-        this.testCase.runTest(new VerifyTableDefinition[] { new VerifyTableDefinition("MY_TABLE",
-                                                                                      null) },
-                              new String[] { "/vito/prove/springbootplusdbunit/ControllerDbTest/myTest/prep/MY_TABLE.csv" },
-                              new String[] { "/vito/prove/springbootplusdbunit/ControllerDbTest/myTest/expected/MY_TABLE.csv" },
-                              () -> {
-                                  try {
-                                      this.controller.modify(new MyTable(null,
-                                                                         "name1",
-                                                                         new Date(100_000L)));
-                                  }
-                                  catch (final Throwable t) {
-                                      throw new Exception(t);
-                                  }
-                                  return null;
-                              });
+        this.dbUnit.runTest(new VerifyTableDefinition[] { new VerifyTableDefinition("MY_TABLE",
+                                                                                    null) },
+                            new String[] { "/vito/prove/springbootplusdbunit/ControllerDbTest/myTest/prep/MY_TABLE.csv" },
+                            new String[] { "/vito/prove/springbootplusdbunit/ControllerDbTest/myTest/expected/MY_TABLE.csv" },
+                            () -> {
+                                try {
+                                    this.controller.modify(new MyTable(null,
+                                                                       "name1",
+                                                                       new Date(100_000L)));
+                                }
+                                catch (final Throwable t) {
+                                    throw new Exception(t);
+                                }
+                                return null;
+                            });
     }
 
     @Test
     void myOtherTest() throws Throwable {
         Assertions.assertFalse(this.controller.findAll().iterator().hasNext());
+    }
+
+    @TestConfiguration
+    static class DbUnitConfiguration {
+        @Autowired
+        DataSource ds;
+
+        @Bean
+        PrepAndExpectedTestCase prepAndExpectedTestCase() {
+            val dt =
+                   new DataSourceDatabaseTester(new TransactionAwareDataSourceProxy(this.ds));
+            dt.setTearDownOperation(DatabaseOperation.DELETE_ALL);
+            return new DefaultPrepAndExpectedTestCase(new CsvDataFileLoader(),
+                                                      dt);
+        }
     }
 }
