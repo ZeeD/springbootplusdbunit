a96b06a45c00689dec3e239dceb8a126da0b42cd
Vito De Tullio <vito.detullio.fincons@skytv.it>
2020-06-30 09:47:31 +0200

    using h2 factory

--- src/test/java/vito/prove/springbootplusdbunit/ControllerDbTest.java
+++ src/test/java/vito/prove/springbootplusdbunit/ControllerDbTest.java
@@ -1,18 +1,10 @@
 package vito.prove.springbootplusdbunit;
 
-import static org.dbunit.database.DatabaseConfig.PROPERTY_DATATYPE_FACTORY;
-
 import java.util.Date;
 
 import javax.sql.DataSource;
 
-import org.dbunit.DataSourceDatabaseTester;
-import org.dbunit.DefaultPrepAndExpectedTestCase;
 import org.dbunit.PrepAndExpectedTestCase;
-import org.dbunit.dataset.datatype.IDataTypeFactory;
-import org.dbunit.ext.h2.H2DataTypeFactory;
-import org.dbunit.operation.DatabaseOperation;
-import org.dbunit.util.fileloader.CsvDataFileLoader;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +12,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 
-import lombok.val;
 import vito.prove.springbootplusdbunit.testsupport.DbUnitHelper;
 
 @SpringBootTest
@@ -36,7 +26,8 @@ public class ControllerDbTest extends DbUnitHelper {
         this.runTest(() -> this.controller.modify(new MyTable(null,
                                                               "name1",
                                                               new Date(100_000L))),
-                     "MY_TABLE");
+                     "MY_TABLE",
+                     "MY_OTHER_TABLE");
     }
 
     @Test
@@ -50,20 +41,8 @@ public class ControllerDbTest extends DbUnitHelper {
         DataSource ds;
 
         @Bean
-        PrepAndExpectedTestCase prepAndExpectedTestCase() throws Exception {
-            val dt =
-                   new DataSourceDatabaseTester(new TransactionAwareDataSourceProxy(this.ds));
-            dt.getConnection()
-              .getConfig()
-              .setProperty(PROPERTY_DATATYPE_FACTORY, new H2DataTypeFactory());
-            dt.setTearDownOperation(DatabaseOperation.DELETE_ALL);
-            return new DefaultPrepAndExpectedTestCase(new CsvDataFileLoader(),
-                                                      dt);
-        }
-
-        @Bean
-        IDataTypeFactory dataTypeFactory() {
-            return new H2DataTypeFactory();
+        PrepAndExpectedTestCase prepAndExpectedTestCase() {
+            return new H2PrepAndExpectedTestCase(this.ds);
         }
     }
 }
--- src/test/java/vito/prove/springbootplusdbunit/testsupport/DbUnitHelper.java
+++ src/test/java/vito/prove/springbootplusdbunit/testsupport/DbUnitHelper.java
@@ -1,19 +1,39 @@
 package vito.prove.springbootplusdbunit.testsupport;
 
+import static java.lang.Thread.currentThread;
 import static java.util.stream.Stream.of;
+import static org.dbunit.database.DatabaseConfig.FEATURE_BATCHED_STATEMENTS;
+import static org.dbunit.database.DatabaseConfig.PROPERTY_DATATYPE_FACTORY;
 
 import java.util.stream.Stream;
 
+import javax.sql.DataSource;
+
+import org.dbunit.DataSourceDatabaseTester;
+import org.dbunit.DefaultPrepAndExpectedTestCase;
+import org.dbunit.IDatabaseTester;
 import org.dbunit.PrepAndExpectedTestCase;
 import org.dbunit.PrepAndExpectedTestCaseSteps;
 import org.dbunit.VerifyTableDefinition;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.ext.h2.H2DataTypeFactory;
+import org.dbunit.operation.DatabaseOperation;
+import org.dbunit.util.fileloader.CsvDataFileLoader;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import lombok.val;
 
 public abstract class DbUnitHelper {
     @Autowired
     PrepAndExpectedTestCase dbUnit;
+
+    static {
+        ((Logger) LoggerFactory.getLogger("org.dbunit")).setLevel(Level.ALL);
+    }
 
     @SuppressWarnings("unchecked")
     public <T> T runTest(final S<T> supplier,
@@ -90,7 +110,7 @@ public abstract class DbUnitHelper {
     }
 
     String testName(final String stem, final String table) {
-        for (val ste : Thread.currentThread().getStackTrace()) {
+        for (val ste : currentThread().getStackTrace()) {
             val cls = ste.getClassName();
             if (!cls.equals(Thread.class.getName()) &&
                 !cls.equals(DbUnitHelper.class.getName()) &&
@@ -103,5 +123,26 @@ public abstract class DbUnitHelper {
                                      table);
         }
         throw new StackOverflowError();
+    }
+
+    public static class H2PrepAndExpectedTestCase extends
+                                                  DefaultPrepAndExpectedTestCase {
+        public H2PrepAndExpectedTestCase(final DataSource ds) {
+            super(new CsvDataFileLoader(), dt(ds));
+        }
+
+        static IDatabaseTester dt(final DataSource ds) {
+            val dt =
+                   new DataSourceDatabaseTester(new TransactionAwareDataSourceProxy(ds));
+            dt.setTearDownOperation(DatabaseOperation.DELETE_ALL);
+            return dt;
+        }
+
+        @Override
+        protected void setUpDatabaseConfig(final DatabaseConfig config) {
+            config.setProperty(FEATURE_BATCHED_STATEMENTS, true);
+            config.setProperty(PROPERTY_DATATYPE_FACTORY,
+                               new H2DataTypeFactory());
+        }
     }
 }
