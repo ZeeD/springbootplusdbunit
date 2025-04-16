ee142961ef8dd1ea5eb008ffb57c1e195605d79e
Vito De Tullio <vito.detullio@finconsgroup.com>
2025-04-16 14:28:36 +0200

    @NoArgsConstructor

--- src/main/java/vito/prove/springbootplusdbunit/entity/MyOtherTable.java
+++ src/main/java/vito/prove/springbootplusdbunit/entity/MyOtherTable.java
@@ -6,9 +6,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
 @AllArgsConstructor(staticName = "of")
 public class MyOtherTable {
     @Id
--- src/main/java/vito/prove/springbootplusdbunit/entity/MyTable.java
+++ src/main/java/vito/prove/springbootplusdbunit/entity/MyTable.java
@@ -7,9 +7,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
 @AllArgsConstructor(staticName = "of")
 public class MyTable {
     @Id
