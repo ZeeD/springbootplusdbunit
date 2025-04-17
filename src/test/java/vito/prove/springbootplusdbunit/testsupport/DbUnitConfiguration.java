dea3f5dfedc9f68fc703885089eaf36dd414da2d
Vito De Tullio <vito.detullio@finconsgroup.com>
2025-04-17 10:48:05 +0200

    flatten

--- src/test/java/vito/prove/springbootplusdbunit/testsupport/DbUnitConfiguration.java
+++ src/test/java/vito/prove/springbootplusdbunit/testsupport/DbUnitConfiguration.java
@@ -13,40 +13,38 @@ import org.dbunit.database.DatabaseConfig;
 import org.dbunit.ext.h2.H2DataTypeFactory;
 import org.dbunit.operation.DatabaseOperation;
 import org.dbunit.util.fileloader.CsvDataFileLoader;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 
+import lombok.RequiredArgsConstructor;
+
+class H2PrepAndExpectedTestCase extends DefaultPrepAndExpectedTestCase {
+    public H2PrepAndExpectedTestCase(final DataSource ds) {
+        super(new CsvDataFileLoader(), dt(ds));
+    }
+
+    static IDatabaseTester dt(final DataSource ds) {
+        final var dsp = new TransactionAwareDataSourceProxy(ds);
+        final var dt = new DataSourceDatabaseTester(dsp);
+        dt.setTearDownOperation(DatabaseOperation.DELETE_ALL);
+        return dt;
+    }
+
+    @Override
+    protected void setUpDatabaseConfig(final DatabaseConfig config) {
+        config.setProperty(FEATURE_BATCHED_STATEMENTS, Boolean.TRUE);
+        config.setProperty(PROPERTY_DATATYPE_FACTORY, new H2DataTypeFactory());
+    }
+}
+
 @Configuration
+@RequiredArgsConstructor
 class DbUnitConfiguration {
-    @Autowired
-    DataSource ds;
+    final DataSource ds;
 
     @Bean
     PrepAndExpectedTestCase prepAndExpectedTestCase() {
         return new H2PrepAndExpectedTestCase(this.ds);
     }
-
-    static class H2PrepAndExpectedTestCase extends
-                                           DefaultPrepAndExpectedTestCase {
-        public H2PrepAndExpectedTestCase(final DataSource ds) {
-            super(new CsvDataFileLoader(), dt(ds));
-        }
-
-        static IDatabaseTester dt(final DataSource ds) {
-            final var dt =
-                         new DataSourceDatabaseTester(new TransactionAwareDataSourceProxy(ds));
-            dt.setTearDownOperation(DatabaseOperation.DELETE_ALL);
-            return dt;
-        }
-
-        @Override
-        protected void setUpDatabaseConfig(final DatabaseConfig config) {
-            config.setProperty(FEATURE_BATCHED_STATEMENTS, Boolean.TRUE);
-            config.setProperty(PROPERTY_DATATYPE_FACTORY,
-                               new H2DataTypeFactory());
-        }
-    }
-
 }
